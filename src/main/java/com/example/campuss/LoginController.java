db0ccbba480269aa5f198f0b9d347360b9434920
mindrutcatalina <catalina.mindrut@student.upt.ro>
2022-05-16 21:57:44 +0300

    CAM-14 Create login functionality for users.

---
+++ src/main/java/com/example/campuss/LoginController.java
@@ -0,0 +1,88 @@
+package com.example.campuss;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+
+public class LoginController implements Initializable {
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+
+    @Override
+    public void initialize(URL url , ResourceBundle resourceBundle){
+        File brandingFile = new File("Imagini/graduation.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("Imagini/padlock.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+    }
+
+    public void loginButtonOnAction(ActionEvent event){
+        loginMessageLabel.setText("You try to login");
+        if(!usernameTextField.getText().isBlank() && !enterPasswordField.getText().isBlank()) {
+            validateLogin();
+        }else{
+            loginMessageLabel.setText("Please enter username and password");
+        }
+    }
+
+    public void cancelButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+
+    }
+    public void validateLogin(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String verifyLogin = " select count(1) from administrator where username = ' " + usernameTextField.getText() +" 'and password =' " + enterPasswordField.getText() +"'";
+
+        try{
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+
+
+            while(queryResult.next()) {
+                if (queryResult.getInt(1) == 1) {
+                    loginMessageLabel.setText("Congratulations!");
+                } else {
+                    loginMessageLabel.setText("Invalid login.Please try again!");
+                }
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.campuss {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.sql;
+    requires mysql.connector.java;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.campuss to javafx.fxml;
+    exports com.example.campuss;
+}
