c6c5f078814aed3addb7936904172dbbbff20460
magda <paicumagda@yahoo.com>
2022-05-24 22:07:04 +0300

    CAM-39 Button between Teacher and GradesAdd

--- src/main/java/com/example/campuss/TeacherController.java
+++ src/main/java/com/example/campuss/TeacherController.java
@@ -53,4 +53,12 @@ public class TeacherController {
         stage.show();
     }
 
+    public void goToGrades(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AddGrades.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
