d59d5981c548aff0332e665fa700d445c1838ae2
mindrutcatalina <102265791+mindrutcatalina@users.noreply.github.com>
2022-05-24 13:49:00 +0300

    CAM -8 As a student , I want to be able to view all courses.

---
+++ src/main/java/com/example/campuss/User2.java
@@ -0,0 +1,15 @@
+package com.example.campuss;
+
+public class User2 {
+
+    private String subjectname;
+
+
+
+    public User2(String subjectname){
+        this.subjectname=subjectname;
+    }
+    public String getSubjectname() {
+        return subjectname;
+    }
+}
